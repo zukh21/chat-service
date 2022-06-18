14cc59f86aa3356e35f9a064b17c46d3de48ccd8
prince-tagaev <asaba@internet.ru>
2022-06-18 12:04:32 +0300

    chat service optimization

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -37,7 +37,7 @@ object WallService{
 
     }
     fun createChat(chat: ChatService): ChatService {
-            if (chat.friend.id != chat.user.id){
+        if (chat.friend.id != chat.user.id){
                 chat.id = ++idForChat
                 directMessages.add(chat)
                 return chat
@@ -45,38 +45,40 @@ object WallService{
         throw UserEqualToNewChatUser("Вы не можете создать чат себе выберите другие пользователи")
     }
 
-    fun getChatsList(){
+    fun getChatsList(): String {
         if (directMessages.isNotEmpty()){
-            for (chat in directMessages){
-                if (chat.message.isEmpty()){
-                    chat.message = arrayOf(defaultMessage)
+            val checkList = directMessages
+                .filter { it.message.isEmpty()
+                    it.message = arrayOf(defaultMessage)
+                    return@filter true
                 }
-                println(chat)
-            }
-        }else throw IsEmpty("У вас нету чаты")
+                .joinToString("\n")
+                .takeWhile { directMessages.isNotEmpty() }
+
+            return checkList
+        }
+        throw IsEmpty("У вас нету чаты")
     }
 
     fun removeChat(chatId: Int): Boolean {
-        for (chat in directMessages){
-            if (chat.id == chatId){
-                directMessages -= chat
-                return true
-            }
-        }
-        throw NotFoundException("Чат с id $chatId не найден")
+            val getChatById = directMessages.getOrElse(chatId-1){throw NotFoundException("Чат с id $chatId не найден")}
+        directMessages -= getChatById
+        return true
     }
 
     fun getUnreadChatsCount(): String {
-        var count = 0
-            for (chat in directMessages){
-                if (chat.isMessageUnread){
-                    count++
-                }
-            }
+        val count = directMessages.count { it.isMessageUnread }
         return "Не прочитанные чаты: $count"
+//        var count = 0
+//            for (chat in directMessages){
+//                if (chat.isMessageUnread){
+//                    count++
+//                }
+//            }
     }
 
     fun createMessage(newMessage: Message, sender: ChatService, recipient: ChatService): Message {
+
         if (newMessage.isUnread && sender.id != recipient.id){
             sender.isMessageUnread = false
             newMessage.isUnread = false
@@ -91,25 +93,33 @@ object WallService{
         throw AllErrorException("Создать сообщения не получился")
     }
 
-    fun editMessage(messId: Int, newMess: Message): Message {
-        for (mess in messages){
-            if (messId == mess.id){
-                messages[messId] = newMess.copy(id = mess.id)
-                messages -= mess
-                return newMess
-            }
+    fun editMessage(messId: Int, newMess: Message): Boolean {
+        messages.filter { it.id.compareTo(messId)
+            messages[messId-1] = newMess.copy(id = messId)
+            messages -= it
+            return true
         }
+//        for (mess in messages){
+//            if (messId == mess.id){
+//                messages[messId] = newMess.copy(id = mess.id)
+//                messages -= mess
+//                return newMess
+//            }
+//        }
         throw NotFoundException("Смс с id $messId не найден")
     }
 
     fun removeMessage(messId: Int, user: ChatService): Boolean {
-        for (mess in messages){
-            if (mess.id == messId && mess in user.message){
-                messages.removeAt(messId-1)
-                return true
-            }
-        }
-        throw NotFoundException("Смс с id $messId не найден или вы хотите чужой смс удалить")
+        val deleteMessage = messages
+            .filter { it.id == messId && it in user.message }
+        messages -= deleteMessage.toSet()
+        return true
+        //        for (mess in messages){
+//            if (mess.id == messId && mess in user.message){
+//                messages.removeAt(messId-1)
+//                return true
+//            }
+//        }
     }
 
     fun getAllMessages(){
@@ -118,6 +128,7 @@ object WallService{
         }
     }
     fun getAllMessagesFromChat(idChat: Int) {
+
         var i = 0
         for (chat in directMessages){
             if (idChat == chat.id){
@@ -130,9 +141,11 @@ object WallService{
             }
 
         }
+
     }
 
 }
+
 
 fun main(){
     val service = WallService
@@ -143,17 +156,24 @@ fun main(){
     val mess1 = Message( mess = "message 1")
     val mess2 = Message( mess = "message 2")
     val mess3 = Message( mess = "message 3")
-    val chat1 = service.createChat(ChatService(user = user, friend = user1, message = arrayOf()))
+    val chat1 = service.createChat(ChatService(user = user, friend = user1, message = arrayOf(), isMessageUnread = true))
     val chat2 = service.createChat(ChatService(user = user, friend = user2, message = arrayOf()))
-    service.createMessage(mess2, chat2, chat1)
-    service.createMessage(mess1, chat2, chat1)
-    service.createMessage(mess3, chat2, chat1)
-    service.getChatsList()
-//    val eMess = service.editMessage(2, mess3)
-//    println(service.removeMessage(2, chat1))
-    service.getAllMessages()
-    service.getChatsList()
-//    service.getAllMessages()
+    println(service.createMessage(mess2, chat2, chat1))
+    println(service.createMessage(mess1, chat2, chat1))
+   println( service.createMessage(mess3, chat2, chat1))
+//    println(service.editMessage(3, mess2))
     println(service.getUnreadChatsCount())
-    service.getAllMessagesFromChat(1)
+    println(service.removeMessage(1, chat1))
+    println(service.getChatsList())
+    println(service.removeChat(1))
+    println(service.getChatsList())
+    println(service.getUnreadChatsCount())
+    service.getAllMessages()
+////    val eMess = service.editMessage(2, mess3)
+////    println(service.removeMessage(2, chat1))
+//    service.getAllMessages()
+//    service.getChatsList()
+////    service.getAllMessages()
+//    println(service.getUnreadChatsCount())
+    service.getAllMessagesFromChat(2)
 }
